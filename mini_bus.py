8dc6a846827e9cc2596f46a1ae0a8079380d31f4
icelemontea003 <icelemontea003@gmail.com>
2018-08-02 22:18:18 +0800

    new searching sysytem

--- mini_bus.py
+++ mini_bus.py
@@ -3,25 +3,49 @@ import pprint
 
 
 line = {
-    '140m':['spiderman',"t'challa",'vision'],
-	    '240m':['bucky','hawkeye','scarlet witch'],
-	    '340m':['superman','mickey','duck','dog'] 
+		    '140M':['spiderman',"t'challa",'vision'],
+			    '240M':['bucky','hawkeye','scarlet witch'],
+			    '340M':['superman','mickey','duck','dog'] 
 }
 
 
-def check(choose_bus):
+def check_stops(choose_bus):
 	if choose_bus in line.keys():
 		print(line[choose_bus])
 	else:
 		print("No This Line !")
 		choose_bus=input("Which mini bus do u want?")
-		check(choose_bus)
+		check_stops(choose_bus)
+
+
+def check_route(station_name):
+	if station_name in line.values():
+		print(line.keys())
+		pprint.pprint("Thank you for searching !")
+	else:
+		pprint.pprint("We dont have mini_bus to go to your target place , sorry")
+
+def check_option(option):
+	if option=='1':
+		choose_bus=input("Which mini bus do u want to search?")
+		check_stops(choose_bus)
+	elif option=='2':
+		station_name=input("Which target station you want to go?")
+		check_route(station_name)
+
 
 
 pprint.pprint(line)
 
-choose_bus=input("Which mini bus do u want?")
-check(choose_bus)
+
+option = input("What do you want to search ?Enter the numbers(1/2)  1.Stops  2.Route number")
+check_option(option)
+
+
+
+
+
+ 	
 
 
 
